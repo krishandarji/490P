ce653902d72ca376ff70499ed49b3e3c1eec16bf
krishandarji <kd283@njit.edu>
2017-05-03 12:48:27 -0400

    updates and stuff

--- RabbitMQServer.php
+++ RabbitMQServer.php
@@ -3,15 +3,55 @@
 require_once('path.inc');
 require_once('host_info.inc');
 require_once('rabbitmqlib.inc');
-
+require_once('DB.inc');
 
 function doLogin($username,$password)
 {
-    // lookup username in databas
-    // check password
-    return true;
-    //return false if not valid
+	$DB = new Database();
+	
+	if (!$DB->connect());
+	{
+		return array("returnCode" => '1', 'message'=>"Connection to Server Failed!");
+	}
+
+	$userinfo = $DB->RetrieveUser($username, $password);
+
+	if ($userinfo)
+	{
+		return (array('returnCode' => '0', 'message'=>'Server received the information!') + $userinfo);
+	}
+	else
+	{
+		return array('returnCode' => '1', 'message'=>'Login Failed! Try Again');
+	}
+
 }
+
+function doRegister($request)
+{
+	$DB = new Database();
+
+	if ($DB->RegisterUser($request['username'], $request['password'], $request['fname'], $request['lname'], $request['email']))
+	{
+		return array("returnCode" => '1', 'message' => "Registered Successfully!");
+	}
+	else
+	{	
+		return array("returnCode" => '0', 'message' => "Registration Failed!<br>User already in database!");
+	}
+)
+
+function logError($request)
+{
+	$log = fopen("logerror.txt", "a");
+
+	fwrite($log, $request['message'] . '\n\n');
+
+	return true;
+}
+
+
+
 
 function requestProcessor($request)
 { 
@@ -23,8 +63,12 @@ function requestProcessor($request)
   }
   switch ($request['type'])
   {
-    case "login":
+    case "Register":
+      return doRegister($request); 
+    case "Login":
       return doLogin($request['username'],$request['password']);
+    case "Log":
+      return logError($request);
     case "validate_session":
       return doValidate($request['sessionId']);
   }
